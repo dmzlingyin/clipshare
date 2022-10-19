a0660fc2851be7618b96b35a523c5164ab798d9c
dmzlingyin <dmzlingyin@163.com>
2022-10-19 22:14:16 +0800

    add client cmd

--- cmd/client.go
+++ cmd/client.go
@@ -6,25 +6,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// daemonCmd represents the daemon command
-var daemonCmd = &cobra.Command{
-	Use:   "daemon",
-	Short: "make the clipboard as a daemon",
+// clientCmd represents the client command
+var clientCmd = &cobra.Command{
+	Use:   "client",
+	Short: "client can detect clipboard change and send the content to server",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("daemon start...")
+		fmt.Println("client called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(daemonCmd)
+	rootCmd.AddCommand(clientCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// daemonCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// clientCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// daemonCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// clientCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
