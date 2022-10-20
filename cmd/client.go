5ade8ff96c8745e8c7b353609e4dbcc14073831c
dmzlingyin <dmzlingyin@163.com>
2022-10-20 23:17:55 +0800

    client to server test done

--- cmd/client.go
+++ cmd/client.go
@@ -2,7 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"net/url"
+	"time"
 
+	"github.com/dmzlingyin/clipshare/pkg/log"
+	"github.com/gorilla/websocket"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +15,7 @@ var clientCmd = &cobra.Command{
 	Use:   "client",
 	Short: "client can detect clipboard change and send the content to server",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("client called")
+		client()
 	},
 }
 
@@ -27,4 +31,24 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// clientCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func client() {
+	fmt.Println("connecting to server...")
+
+	u := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/websocket"}
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.ErrorLogger.Println(err)
+	}
+	defer c.Close()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for t := range ticker.C {
+		err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+		if err != nil {
+			log.ErrorLogger.Println(err)
+		}
+	}
 }
